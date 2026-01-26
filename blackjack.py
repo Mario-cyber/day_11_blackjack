300d53844cb4cc62825716e49eb25c74a76e7212
Mario_F <54963398+Mario-cyber@users.noreply.github.com>
2026-01-26 19:11:42 +0000

    deck and varibales created

---
+++ blackjack.py
@@ -0,0 +1,13 @@
+# genenerate deck
+
+cards =  [11,2,3,4,5,6,7,8,9,10,10,10,10]
+
+# generate variables to be populated
+
+player_hand = []
+dealer_hand = []
+
+player_score = []
+dealer_score = []
+
+
