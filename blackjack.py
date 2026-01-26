c9db792411c39b39cdb6ad11c0581145eecd52bf
Mario_F <54963398+Mario-cyber@users.noreply.github.com>
2026-01-26 20:25:59 +0000

    initial deal worked out

--- blackjack.py
+++ blackjack.py
@@ -1,3 +1,4 @@
+import random
 # genenerate deck
 
 cards =  [11,2,3,4,5,6,7,8,9,10,10,10,10]
@@ -11,3 +12,15 @@ player_score = []
 dealer_score = []
 
 
+iterator = 0
+
+while iterator < 2:
+    player_hand.append(cards[random.randint(0,len(cards))])
+    dealer_hand.append(cards[random.randint(0,len(cards))]) 
+    iterator += 1
+
+player_score = sum(player_hand)
+dealer_score = sum(dealer_hand)
+
+print(player_hand, player_score)
+print(dealer_hand, dealer_score)
