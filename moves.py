f29eae55e29790333729707c7fee438f871546e0
Mario_F <54963398+Mario-cyber@users.noreply.github.com>
2026-01-27 21:49:46 +0000

    'hit' function created

---
+++ moves.py
@@ -0,0 +1,20 @@
+import random
+cards =  [11,2,3,4,5,6,7,8,9,10,10,10,10]
+
+def first_deal(player_hand, dealer_hand):
+     iterator = 0 
+    
+     while iterator < 2:
+     
+        player_hand.append(cards[random.randint(0,len(cards)-1)])
+        dealer_hand.append(cards[random.randint(0,len(cards)-1)])
+        
+
+        iterator += 1
+
+
+
+def hit(hand):
+    hand.append(cards[random.randint(0,len(cards)-1)])
+    return hand
+    
