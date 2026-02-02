21625097876b6e255caa14931492f9ef3ff03625
Mario_F <54963398+Mario-cyber@users.noreply.github.com>
2026-02-02 21:31:27 +0000

    working on aeshetics

--- blackjack.py
+++ blackjack.py
@@ -17,24 +17,12 @@ dealer_score = sum(dealer_hand)
 
 print(player_hand, player_score)
 
-# after funcionality is done, fix this display to only one card
-# print(dealer_hand[0])
-print(dealer_hand, dealer_score)
 
-# check for blackjack
-# it could be the case that both player and dealer hit blackjack in the first deal. That's a push. 
-# check for dealer blackjack first if the dealer is under 
+print(dealer_hand[0])
+
+
 
 continue_playing =  True
-
-# you will have to fix this later to give the dealer or player a chance to "push"
-if dealer_score == 21 :
-    print("you lose")
-    continue_playing = False
-elif player_score == 21 :
-    print("you win")
-    continue_playing = False
-
 
 
 while continue_playing == True:
@@ -75,18 +63,7 @@ else:
         print(f"final score: \n Player: {player_hand}-> {player_score} \nDealer: {dealer_hand} -> {dealer_score}")
         if dealer_score > player_score:
             print("you lose !")
-        elif
-        # this part needs fine tuninng 
-        # if player_score > dealer_score:
-        #     print("you win, player!")
-        #     continue_playing = False
-        # elif player_score < dealer_score:
-        #     print("you lose player")
-        #     continue_playing = False
-        # elif player_score == dealer_score :
-        #     print("it's a push")
-        #     continue_playing = False
-
-#remainig To Do's: 
-#display only one of the dealers hands at the begining of the game 
-#fix check for bust option so that the right message is displayed
+        elif dealer_score < player_score:
+            print("you win")
+        else:
+            print("it's a push")
