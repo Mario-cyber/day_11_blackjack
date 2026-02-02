3004c7ef49c4ec9b3e4323ea0e8219e9c235f07c
Mario_F <54963398+Mario-cyber@users.noreply.github.com>
2026-02-02 19:48:04 +0000

    sandbox used to toy around

--- sandbox.py
+++ sandbox.py
@@ -1,9 +1,84 @@
-hand  = [11,2,3,4,5,6]
+import moves
 
-print(hand)
+# generate variables to be populated
 
-if 11 in hand :
-    print(hand.index(11))
-    hand[hand.index(11)] = 1 
-    print(hand)
-    print(hand[0])
+player_hand = []
+dealer_hand = []
+
+player_score = []
+dealer_score = []
+
+# deal first 2 cards
+
+moves.first_deal(player_hand=player_hand, dealer_hand=dealer_hand)
+
+player_score = sum(player_hand)
+dealer_score = sum(dealer_hand)
+
+print(player_hand, player_score)
+
+# after funcionality is done, fix this display to only one card
+# print(dealer_hand[0])
+print(dealer_hand, dealer_score)
+
+# check for blackjack
+# it could be the case that both player and dealer hit blackjack in the first deal. That's a push. 
+# check for dealer blackjack first if the dealer is under 
+
+continue_playing =  True
+
+# you will have to fix this later to give the dealer or player a chance to "push"
+if dealer_score == 21 :
+    print("you lose")
+    continue_playing = False
+elif player_score == 21 :
+    print("you win")
+    continue_playing = False
+
+
+
+while continue_playing == True:
+    another_card = input("do you want another card? y/n: ")
+
+    if another_card == 'y':
+        # deal another card to player 
+        moves.hit(hand=player_hand)
+        player_score = moves.calculate_score(hand = player_hand)
+        print(player_hand, player_score)
+        continue_playing = moves.check_for_bust_player(score=player_score)
+        print(f"here is what is ahppening here: continue_playing = {continue_playing}")
+        if player_score == 21:
+            print("you win, player!")
+            
+        
+    elif another_card == 'n':
+        print(player_hand, player_score)
+        continue_playing  = False
+
+
+#gotta split the loop here, otherwise, if the player busts, the dealer can still play 
+
+while 17 > dealer_score < 21 :
+    #deal another card to dealer
+    moves.hit(hand = dealer_hand)
+    dealer_score = moves.calculate_score(hand=dealer_hand) 
+    print(dealer_hand,dealer_score)
+    #continue_playing = moves.check_for_bust_dealer(score = dealer_score)
+
+if dealer_score > 21 : 
+    print("dealer loses")
+    
+    # this part needs fine tuninng 
+    # if player_score > dealer_score:
+    #     print("you win, player!")
+    #     continue_playing = False
+    # elif player_score < dealer_score:
+    #     print("you lose player")
+    #     continue_playing = False
+    # elif player_score == dealer_score :
+    #     print("it's a push")
+    #     continue_playing = False
+
+#remainig To Do's: 
+#display only one of the dealers hands at the begining of the game 
+#fix check for bust option so that the right message is displayed
